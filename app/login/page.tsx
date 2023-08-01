e429efa2893e7aa27f3dfaa3a178516398142ccb
dzoniD <edvard_dantes@hotmail.com>
2023-08-01 22:24:28 +0200

    login css fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,10 @@
-
 import Image from "next/image";
-import { Login } from "@/components/Login/Login"
+import { Login } from "@/components/Login/Login";
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex min-h-screen flex-col items-center justify-evenly mx-5">
       Login Page
-      <Login/>
-
+      <Login />
     </main>
   );
 }
